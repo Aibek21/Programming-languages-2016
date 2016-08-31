c573a5cdbcab45e2cc0cdecc96a89715276c2016
githubBobur <bobur.shoh.94@mail.ru>
2016-08-31 10:30:39 +0600

    first commit

---
+++ week2/e.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){	
+
+	string first_name;
+	string firstName;
+	string _name;
+	string name_2;
+	//string 2_name  ===> wrong name
+	
+	int number;
+	int number2;
+	int my_number;
+	
+
+return 0;
+
+}
