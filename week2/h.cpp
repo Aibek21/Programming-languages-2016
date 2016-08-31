3c629ab1ea5613f7531dda821fe8bcc37a813ff5
githubBobur <bobur.shoh.94@mail.ru>
2016-08-31 18:49:29 +0600

    storing string

---
+++ week2/h.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+		
+	string first_name = "Arman";
+	string last_name = "Armanov";
+
+	string s = "Hello " + first_name + " " + last_name + ".";
+
+	cout << s;
+
+
+return 0;
+}
