d293fde31d99418bcf15ded2b25ff16d187b2457
githubBobur <bobur.shoh.94@mail.ru>
2016-08-31 19:44:14 +0600

    sin, cos, tan

---
+++ week2/j.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+
+
+	cout << "sin(90) = " << sin(90*M_PI/180) << endl;
+	cout << "cos(0) = " << cos(0*M_PI/180) << endl;
+	cout << "tan(45) = " << tan(45*M_PI/180) << endl; 
+	cout << "atan(45) = " << tan(45*M_PI/180) << endl; //arc tangent is the inverse operation of tangent
+
+
+return 0;
+}
