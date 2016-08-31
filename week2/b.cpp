3e0e94582452831f686e51ed0a0f3a83312ae105
githubBobur <bobur.shoh.94@mail.ru>
2016-08-31 10:24:26 +0600

    first commit

---
+++ week2/b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){	     
+	
+	cout << "Hello\n";
+	printf("%s", "Hello");
+
+		
+
+return 0;
+
+}
---
+++ week2/c.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){	     
+	
+	int a = 10;
+	int b = 3;
+	
+	cout << a/b; //prints as integer
+
+	double a = 10;
+	double b = 3;
+	
+	cout << a/b; //prints as double with fraction
+	printf("%.5f", a/b);
+		
+
+return 0;
+
+}
