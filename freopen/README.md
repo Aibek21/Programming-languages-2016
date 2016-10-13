4ba556d0649c18edaa494a59b496111002cd36f7
githubBobur <bobur.shoh.94@mail.ru>
2016-10-13 12:26:00 +0600

    read and write

---
+++ freopen/README.md
@@ -0,0 +1,3 @@
+# Read from file and Write to file
+- compile your code `g++ a.cpp`
+- run your a.exe file with input.txt file `a.exe < input.txt`
---
+++ freopen/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+int main(){
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	
+	int n;
+	
+	cin >> n;
+
+
+	cout <<n << " is read from input.txt file";
+
+return 0;
+}
