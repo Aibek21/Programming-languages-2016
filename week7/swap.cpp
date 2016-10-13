cf25f30781d0e840eab86781460abe6ebfa3d8f0
githubBobur <bobur.shoh.94@mail.ru>
2016-10-13 12:41:25 +0600

    swap function

---
+++ week7/swap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a = 2;
+	int b = 3;
+
+	swap(a, b);
+	
+    /*
+		int k = a;
+		a = b;
+		b = k;
+    */
+
+	cout << a << " " << b;
+
+return 0;
+}
+                 
