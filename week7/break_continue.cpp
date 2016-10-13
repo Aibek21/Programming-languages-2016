d3dec52108fc8693a16d408ec1e1dbf39cb877b9
githubBobur <bobur.shoh.94@mail.ru>
2016-10-13 14:21:48 +0600

    break and continue

---
+++ week7/break_continue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	for(int i = 1; i <= 10; i++){
+		if( i == 6 ) break;
+		cout << i << " ";
+	}
+
+	cout << "\n\n";
+
+
+	for(int i = 1; i <= 10; i++){
+		if( i == 6 ) continue;
+		cout << i << " ";
+	}
+
+
+return 0;
+}
+                 
